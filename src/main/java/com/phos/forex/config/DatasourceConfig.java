82d48003fa418c632f92d896f6ad67860c2cc7ef
Adefolarin Osu <folzieds@gmail.com>
2021-01-14 02:38:30 +0100

    Added datasource configuration

---
+++ src/main/java/com/phos/forex/config/DatasourceConfig.java
@@ -0,0 +1,39 @@
+package com.phos.forex.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by Folarin on 14/01/2021
+ */
+@Configuration
+public class DatasourceConfig {
+
+    @Value("${datasource.driver-class-name}")
+    private String driverClassName;
+
+    @Value("${phos.datasource.url}")
+    private String dataSourceUrl;
+
+    @Value("${phos.datasource.username}")
+    private String dataSourceUsername;
+
+    @Value("${phos.datasource.password}")
+    private String dataSourcePassword;
+
+    @Bean
+    public DataSource phosDataSource(){
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(dataSourceUrl);
+        dataSource.setUsername(dataSourceUsername);
+        dataSource.setPassword(dataSourcePassword);
+
+        return dataSource;
+    }
+}
